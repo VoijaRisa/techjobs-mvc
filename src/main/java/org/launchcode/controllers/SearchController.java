f35e98d452e2359a11f2e894f99dc2914dfc8db6
Jon Voisey <VoijaRisa@gmail.com>
2018-05-30 17:52:24 -0500

    Finished basic work. TODO: Get # of results on list pages.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,8 +4,10 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,6 +24,28 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String processSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> searchResults = new ArrayList<>();
+
+        if (searchType.equals("all")) {
+            searchResults = JobData.findByValue(searchTerm);
+            searchType = "WTF";
+        }
+        else {
+            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        Integer numResults = searchResults.size();
+
+        model.addAttribute("searchresults", searchResults);
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("numResults", numResults + " Result(s)");
+
+        return "search";
+    }
 
 }
